9865fee9f0b5ef7d6c2b247199722bc19ac4cf51
fazt <fazt@faztweb.com>
2023-03-20 23:58:11 -0500

    metadata added

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,10 +3,13 @@ import { Layout } from "../components/Layout";
 import { Navbar } from "../components/Navbar";
 import { TasksProvider } from "../context/TasksContext";
 
+export const metadata = {
+  title: "Tasks App",
+}
+
 export default function RootLayout({ children }) {
   return (
     <html>
-      <head />
       <body>
         <TasksProvider>
           <Navbar />
